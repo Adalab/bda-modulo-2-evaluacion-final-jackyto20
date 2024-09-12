6ea8476ff311e71114c12e5a66b12724e4b1186b
Jackyto20 <jackeline_yusti@yahoo.com>
2024-09-12 18:45:23 +0200

    Evaluacion Terminada BBD SQL

--- evaluacion_jackie.sql
+++ evaluacion_jackie.sql
@@ -32,11 +32,11 @@ from actor;
 
 SELECT first_name AS NombreActor, last_name AS ApellidoActor
 FROM actor
-WHERE last_name = 'GIBSON';
+WHERE last_name LIKE '%GIBSON%';
 
 -- 7. Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20 +++.
 
-SELECT first_name AS NombreApellido
+SELECT first_name AS NombreActor
 FROM actor 
 WHERE actor_id BETWEEN 10 AND 20;
 
@@ -60,17 +60,17 @@ SELECT C.customer_id AS IDCliente, CONCAT(C.first_name, ' ', C.last_name) AS Nom
 FROM customer C -- Datos del cliente
 INNER JOIN rental R -- Tengo los aquileres
 ON C.customer_id = R.customer_id
-GROUP BY C.customer_id;
+GROUP BY C.customer_id, C.first_name, C.last_name;
 
 
 /* 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el
 recuento de alquileres. +++*/
 
 SELECT C.name AS NombreCategoria, COUNT(R.rental_id) AS TotalPeliculasAlquiladas
-FROM rental R
-INNER JOIN inventory I -- Unir rental con inventario para saber que copia de peli se alquilo
+FROM rental R -- Nos indica que inventario fue alquilado
+INNER JOIN inventory I -- Unir rental con inventario para saber que copia de peli y unida a rental sabemos que peli se alquilo
 ON R.inventory_id = I.inventory_id
-INNER JOIN film_category FY -- Unir inventario con categoria para saber que pelicula fue alquilada segun cat
+INNER JOIN film_category FY -- Unir inventario con categoria para saber que pelicula fue alquilada segun categoria
 ON  I.film_id = FY.film_id
 INNER JOIN category C -- Unimos a categoria para saber el nombre de la categoria 
 ON FY.category_id = C.category_id
@@ -105,13 +105,13 @@ SELECT  A.actor_id AS IDActor, A.first_name AS NombreActor
 FROM actor A 
 LEFT JOIN film_actor FA -- Necesitamos traer todos los actores de la tabla actor incluso si no esta en la tabla film_actor
 ON A.actor_id = FA.actor_id
-WHERE FA.actor_id IS NOT NULL;
+WHERE FA.actor_id IS NULL;
 
 -- 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010 +++.
 
-SELECT title AS TituloPelicula, release_year
+SELECT title AS TituloPelicula
 FROM film
-WHERE release_year BETWEEN '2005' AND '2010';
+WHERE release_year BETWEEN 2005 AND 2010;
 
 -- 17. Encuentra el título de todas las películas que son de la misma categoría que "Family" +++.
 
@@ -123,6 +123,17 @@ INNER JOIN category C
 ON C.category_id = FC.category_id
 WHERE C.name = 'Family';
 
+-- Buscar el id de categoria Family y luego las peliculas que pertenezcan a esa categoria
+SELECT F.title AS TituloPelicula, C.name
+FROM film F
+INNER JOIN film_category FC ON F.film_id = FC.film_id
+INNER JOIN category C ON C.category_id = FC.category_id
+WHERE C.category_id = (
+    SELECT C2.category_id
+    FROM category C2
+    WHERE C2.name = 'Family'
+);
+
 -- 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas +++.
 
 SELECT CONCAT(A.first_name, " " , A.last_name) AS NombreActor
@@ -130,13 +141,13 @@ FROM actor A
 INNER JOIN film_actor FA
 ON A.actor_id = FA.actor_id
 GROUP BY A.first_name, A.last_name
-HAVING COUNT(FA.actor_id) > 10; -- Cuenta los actores segun la pelicula
+HAVING COUNT(FA.film_id) > 10; -- Cuenta el numero de peliculas por actor
 
 -- 19. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film +++.
 
 SELECT title AS TituloPelicula
 FROM film
-WHERE rating = 'R' AND length > 160;
+WHERE rating = 'R' AND length > 120;
 
 /* 20. Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y muestra el
 nombre de la categoría junto con el promedio de duración.+++*/
@@ -153,19 +164,25 @@ HAVING PromedioDuracion > 120;
 /* 21. Encuentra los actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con la cantidad
 de películas en las que han actuado--.+++*/
 
-SELECT CONCAT(A.first_name, " " , A.last_name) AS NombreActor, COUNT(FA.film_id) AS TotalPeliculas
+SELECT CONCAT(A.first_name, " " , A.last_name) AS NombreActor, COUNT(FA.film_id) AS TotalPeliculasActor
 FROM actor A
 INNER JOIN film_actor FA
 ON A.actor_id = FA.actor_id
 GROUP BY A.first_name, A.last_name
-HAVING TotalPeliculas >= 5;
+HAVING COUNT(FA.film_id) >= 5;
 
 
 /* 22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una subconsulta para
 encontrar los rental_ids con una duración superior a 5 días y luego selecciona las películas correspondientes.*/
 
-
-
+SELECT title AS TituloPelicula
+FROM film
+WHERE film_id IN(
+SELECT I.film_id
+FROM rental R
+INNER JOIN inventory I
+ON R.rental_id = I.inventory_id
+WHERE DATEDIFF(R.return_date, R.rental_date) >5);
 
 
 /* 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror".
@@ -192,18 +209,22 @@ INNER JOIN film_category FY
 ON F.film_id = FY.film_id
 INNER JOIN category C 
 ON C.category_id = FY.category_id
-WHERE C.name = 'comedy' and length > 120;
+WHERE C.name = 'comedy' and length > 180;
 
 /* 25. BONUS: Encuentra todos los actores que han actuado juntos en al menos una película. La consulta debe mostrar el
 nombre y apellido de los actores y el número de películas en las que han actuado juntos. CHAT GPT */ 
 
-SELECT CONCAT(A1.first_name, " " , A1.last_name) AS NombreActor1,  CONCAT(A2.first_name, " " , A2.last_name) AS NombreActor2, COUNT(*) AS TotalPeliculasJuntos
+WITH ParejasActores AS(
+SELECT FA1.actor_id AS Actor1_ID, FA2.actor_id AS Actor2_ID, COUNT(FA1.film_id) AS TotalPeliculasJuntos
 FROM film_actor FA1
 INNER JOIN film_actor FA2
 ON FA1.film_id = FA2.film_id
-INNER JOIN actor A1 
-ON FA1.actor_id = A1.actor_id
-INNER JOIN actor A2
-ON FA2.actor_id = A2.actor_id
-GROUP BY A1.actor_id, A2.actor_id, A1.first_name, A1.last_name, A2.first_name, A2.last_name
-HAVING COUNT(*) >0;
+AND FA1.actor_id < FA2.actor_id 
+GROUP BY FA1.actor_id, FA2.actor_id)
+
+SELECT CONCAT(A1.first_name, " " , A1.last_name) AS NombreActor1,  CONCAT(A2.first_name, " " , A2.last_name) AS NombreActor2, AP.TotalPeliculasJuntos
+FROM ParejasActores AP
+INNER JOIN actor A1 ON AP.Actor1_ID = A1.actor_id
+INNER JOIN actor A2 ON AP.Actor2_ID = A2.actor_id
+ORDER BY AP.TotalPeliculasJuntos DESC;
+
